2c4997ee0c46a1c2290af91204e3a246c00cf439
Sandeep Banoula <sandeep.banoula@gmail.com>
2024-05-11 00:28:48 +0530

    feat: create dashboard view
    - created dashboard view that will show all notebooks of user either joined or created
    - defined extra check if user has already joined the Notebook
    - defined selectUserNotebook query on UserNotebookRepository.js
    - focus input on create or join Notebook modal open
    - define underscored: true in sequelize
    - removed firstrun dependency note form README.md
    
    refs: #25

--- server/repository/UserNotebookRepository.js
+++ server/repository/UserNotebookRepository.js
@@ -1,14 +1,54 @@
-const { UserNotebook } = require("../model/model");
+const { UserNotebook, Notebook, User } = require("../model/model");
 
 const createUserNotebook = async (nbUserId, nbId, nbRole, transaction) => {
     const userNb = await UserNotebook.create({
         user_id: nbUserId,
         notebook_id: nbId,
         role: nbRole,
-    },{
+    }, {
         transaction: transaction
     })
     return userNb;
 }
 
-module.exports = { createUserNotebook }
+const findCreateUserNotebook = async (nbUserId, nbId, nbRole, transaction, rw) => {
+    const [userNotebook, created] = await UserNotebook.findOrCreate({
+        where: {
+            user_id: nbUserId,
+            notebook_id: nbId
+        },
+        defaults: {
+            user_id: nbUserId,
+            notebook_id: nbId,
+            role: nbRole,
+        },
+        transaction: transaction,
+        raw: rw
+    });
+    return [userNotebook, created];
+}
+
+const selectUserNotebook = async (userId, rw) => {
+    const userNb = await UserNotebook.findAll({
+        include: [{
+            model: Notebook,
+            include: [{
+                model: User,
+                required: true,
+                right: true,
+                attributes: ["name", "pic"]
+            }],
+            required: true,
+            right: true,
+            attributes: ["name", "color", "code", "createdAt"],
+        }],
+        where: {
+            user_id: userId
+        },
+        attributes: ["role", "notebook_id"],
+        raw: rw
+    });
+    return userNb;
+}
+
+module.exports = { createUserNotebook, findCreateUserNotebook, selectUserNotebook }
